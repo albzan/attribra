6e92751ba23aeb57fb704616d0834d73741f8f49
albzan <lapostadialberto@gmail.com>
2017-05-27 13:36:20 +0200

    first release

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # attribra
-NVDA Add-On to add Braille Attributes Support
+NVDA Add-On that adds Braille Attributes capability
+
+This NVDA Add-On gives the possibility to the user to define custom rules to mark text with special fields option with braille dots 7 and 8.
+In short you can mark bold, italic, specific foreground or background colors etc. using dots 7 and 8 of your refreshable braille display.
+
+Features provided by this addon are **independent** from the Document Formatting option: i.e. you can mark spelling errors without have the option selected on the Document Formatting dialog.
+
+It supports both application-specific or global configuration rules.
+In order to edit rules you have to manually edit the attribra.ini configuration file.
+
+Examples of configuration rules are:
+
+```
+[winword]
+invalid-spelling = 1
+```
+
+Braille marking of spelling errors in Word
+
+```
+[eclipse]
+"background-color" = "rgb(24420045)", "rgb(2550128)"
+```
+
+Marks errors and warnings in braille while using the Eclipse IDE.
+
+```
+[firefox]
+color = "RGB(255,0,0)"
+```
+
+Braille marking of red text
+
+```
+[global]
+bold = 1
+```
+
+Marks bold text in all applications excepts firefox, eclipse and winword for which a specific configuration exists
+
+To determine the name and the value of the rules you need, you can use the "debug mode".
+To toggle debug mode press **Control+Shift+NVDA+B**
+This will log all the information about the text while you move around. Pleas usse this option carefully and only for a short amount of time, for instance when you have already located a text with the pattern you want to be marked.
+After that, toggle this option off and review the log from the NVDA tools menu.
+Identify the property you want to mark and report it in the attribra configuration file.
+
+
+##Add-On Shortcuts:
+* **NVDA+Control+B**: Open the attribra.ini file with your default INI editor
+* **NVDA+Control+Shift+B**: Toggle debug logging on or / off
+
+##Author
+Alberto Zanella <lapostadi[myfirstname]#gmail.com>
---
+++ addon/globalPlugins/attribra.py
@@ -0,0 +1,125 @@
+import globalPluginHandler
+import appModuleHandler
+import braille
+import api
+import ui
+import textInfos
+from logHandler import log
+from configobj import ConfigObj #INI file parsing
+from colors import RGB
+import os
+import globalVars
+
+ATTRS = {}
+logTextInfo = False
+
+def decorator(fn,str) :
+    def _getTypeformFromFormatField(self, field):
+        #convention: to mark we put 4 (bold for liblouis)
+        for attr,value in ATTRS.iteritems() :
+            fval = field.get(attr,False)
+            if fval in value :
+                return 4
+        # if COMPLCOLORS != None :
+            # col = field.get("color",False)
+            # if col and (col != COMPLCOLORS):
+                # return 4
+        return 0
+    
+    def addTextWithFields_edit(self, info, formatConfig, isSelection=False):
+        conf = formatConfig.copy()
+        conf["reportFontAttributes"]=True
+        conf["reportColor"]=True
+        conf["reportSpellingErrors"]=True
+        if logTextInfo :
+            log.info(info.getTextWithFields(conf))
+        fn(self, info, conf, isSelection)
+
+
+    def update(self) :
+        fn(self)
+        DOT7 = 64
+        DOT8 = 128
+        for i in xrange(0,len(self.rawTextTypeforms)) :
+            if self.rawTextTypeforms[i] == 4 :
+                self.brailleCells[i] |= DOT7 | DOT8
+
+    if str == "addTextWithFields" :
+        return addTextWithFields_edit
+    if str == "update" :
+        return update
+    if str == "_getTypeformFromFormatField" :
+        return _getTypeformFromFormatField
+
+
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+    configs = {}
+    currentPid = ""
+    def event_gainFocus(self,obj,nextHandler) :
+        nextHandler()
+        pid = obj.processID
+        if self.currentPid != pid :
+            self.populateAttrs(pid)
+            self.currentPid = pid
+    
+    def populateAttrs(self,pid) :
+        if (len(self.configs) == 0) : return
+        global ATTRS #We are changing the global variable
+        appname = appModuleHandler.getAppNameFromProcessID(pid)
+        if (appname in self.configs) :
+            ATTRS = self.configs[appname]
+        elif ("global" in self.configs) :
+            ATTRS = self.configs["global"]
+        else : ATTRS = {}
+        
+    
+    def __init__(self):
+        self.configFile = os.path.join(globalVars.appArgs.configPath,"addons","attribra", "attribra.ini")
+        self.parsecfgs() #parse configuration
+        if len(self.configs) > 0 : #If no cfg then do not replace functions
+            braille.TextInfoRegion._addTextWithFields = decorator(braille.TextInfoRegion._addTextWithFields,"addTextWithFields")
+            braille.TextInfoRegion.update = decorator(braille.TextInfoRegion.update,"update")
+            braille.TextInfoRegion._getTypeformFromFormatField = decorator(braille.TextInfoRegion._getTypeformFromFormatField,"_getTypeformFromFormatField")        
+            
+        #superclass constr.
+        return super(GlobalPlugin,self).__init__()
+    
+    def parsecfgs(self) :
+        try :
+            config = ConfigObj(self.configFile, encoding="UTF-8")
+            for app,map in config.iteritems() :
+                mappings = {}
+                for name,value in map.iteritems() :
+                    if isinstance(value,basestring) :
+                        if value.startswith("RGB(") : #it's an RGB Object
+                            rgbval = value.split("RGB(")[1]
+                            rgbval = rgbval.split(")")[0]
+                            rgbval = rgbval.split(",")
+                            mappings[name] = [RGB(int(rgbval[0]),int(rgbval[1]),int(rgbval[2]))]
+                        else : 
+                            try :
+                                #if possible adds the value and its int
+                                mappings[name] = [value,int(value)]
+                            except ValueError :
+                                mappings[name] = [value]
+                    else : mappings[name] = value
+                self.configs[app] = mappings
+        except IOError:
+            log.debugWarning("No attribra.ini found")
+    
+    #TODO
+    def script_editConfig(self,gesture) :
+        self.parsecfgs()
+        os.system("start "+self.configFile)
+    
+    def script_logFieldsAtCursor(self,gesture) :
+        global logTextInfo
+        logTextInfo = not logTextInfo
+        msg = ["stop","start"]
+        ui.message("debug textInfo "+msg[logTextInfo])
+        
+    __gestures={
+        "kb:NVDA+control+a":"editConfig",
+        "kb:NVDA+control+shift+a":"logFieldsAtCursor",
+    }
