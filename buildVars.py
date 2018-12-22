84baad451dc3afa6fab053c644975d42115fd5ac
albzan <lapostadialberto@gmail.com>
2018-12-22 10:29:01 +0100

    updated buildvars

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Provide Braille Attributes support to mark relevant text with dots 7 and 8"""),
 	# version
-	"addon_version" : "0.1",
+	"addon_version" : "0.2",
 	# Author(s)
 	"addon_author" : u"Alberto Zanella <lapostadialberto@gmail.com>",
 	# URL for the add-on documentation support
